0cf6d108f624cc5aeefa630c907a92d81a814573
Arya007Sharma <146719956+Arya007Sharma@users.noreply.github.com>
2025-05-29 10:10:26 -0400

    Updated Ports  app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import psutil
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -12,5 +13,6 @@ def index():
         Message = "High CPU or Memory Detected, scale up!!!"
     return render_template("index.html", cpu_metric=cpu_metric, mem_metric=mem_metric, message=Message)
 
-if __name__=='__main__':
-    app.run(debug=True, host = '0.0.0.0')
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
